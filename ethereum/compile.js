44c73ae22eb14b2a453c4a76fe45b855a37c2f54
dtutila <daniel.tutila@gmail.com>
2018-01-27 16:34:35 -0600

    compile

---
+++ ethereum/compile.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campaingPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync( campaingPath, 'utf8');
+
+const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath);
+
+for(let contract in output){
+	fs.outputJsonSync(
+			path.resolve(buildPath, contract.replace(':', '') + '.json'),
+			output[contract]
+		);
+}
