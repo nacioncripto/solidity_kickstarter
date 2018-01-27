8fdb192deca0630fe4ced119ff55f0eebbc263da
Douglas Molina <douglas.molina@intertecintl.com>
2018-01-27 17:35:26 -0600

    add contributor test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -40,8 +40,21 @@ describe('Campaigns', () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
+
   it('marks caller as the campaign manager', async () => {
     const manager = await campaign.methods.manager().call();
     assert.equal(accounts[0], manager);
   });
+
+  it('allows to contribute money and marks them as approvers', async () => {
+    await campaign.methods.contribute().send({
+      value: '200',
+      from: accounts[1]
+    });
+
+    const isContributor = await campaign.methods.approvers(accounts[1]).call();
+    assert(isContributor);
+  });
+
+
 })
