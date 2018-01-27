b31ddabf809d46de1d25eeccc79fdadfbb04308d
dtutila <daniel.tutila@gmail.com>
2018-01-27 17:10:53 -0600

    adding test

---
+++ test/Campaign.test.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+const compiledFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+
+let accounts;
+let factory;
+let campaign;
+let campaignAddress;
+
+beforeEach( async ()=> {
+	accounts = await web3.eth.getAccounts();
+
+	factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ from: accounts[0], gas: '1000000' });
+
+    factory.setProvider(provider);
+
+    await factory.methods.createCampaign('100').send({
+    	from: accounts[0],
+    	gas: '1000000'
+    });
+
+    [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+    campaign = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface),
+    campaignAddress
+  );
+});
+
+
+describe('Campaigns', () => {
+	it('deploys a factory and a campaign', () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
+})
