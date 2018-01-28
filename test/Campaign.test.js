a096fa9a3a5ddc1042f1a96ac770d3de090f51d3
Douglas Molina <douglas.molina@intertecintl.com>
2018-01-27 18:01:24 -0600

    add request test contributor

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -68,4 +68,45 @@ describe('Campaigns', () => {
     }
   });
 
+  it('allows manager to make a payment request', async () => {
+    await campaign.methods.createRequest('Buy batteries', '100', accounts[1])
+                  .send({
+                    from: accounts[0],
+                    gas: '1000000'  
+                  });
+    
+    const request = await campaign.methods.requests(0).call();
+
+    assert.equal('Buy batteries', request.description);  
+  });
+
+  it('processes requests', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether')
+    });
+
+    await campaign.methods
+            .createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+            .send({ from: accounts[0], gas: '1000000' });
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+    
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, 'ether');
+    balance = parseFloat(balance);
+
+    assert(balance > 104);
+
+    
+  });
+
 })
